03178d37de15883161c23402abf90b552a95c339
JCrest <1554687045@qq.com>
2017-06-02 20:40:15 +0800

    引导页面View Pager 添加3张广告图片

--- 北京新闻/src/main/java/com/example/jiangchuanfa/beijingnews2rd/Activity/GuideActivity.java
+++ 北京新闻/src/main/java/com/example/jiangchuanfa/beijingnews2rd/Activity/GuideActivity.java
@@ -1,13 +1,19 @@
 package com.example.jiangchuanfa.beijingnews2rd.Activity;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.example.jiangchuanfa.beijingnews2rd.R;
+
+import java.util.ArrayList;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -24,14 +30,58 @@ public class GuideActivity extends AppCompatActivity {
     @InjectView(R.id.activity_guide)
     RelativeLayout activityGuide;
 
+    ArrayList<ImageView> imageViews;
+    int[] ids = {R.drawable.guide_1, R.drawable.guide_2, R.drawable.guide_3};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide);
         ButterKnife.inject(this);
+
+        initData();
+        vp.setAdapter(new MyAdapter());
     }
+
+    private void initData() {
+        imageViews = new ArrayList<>();
+        for (int i = 0; i < ids.length; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setBackgroundResource(ids[i]);
+            imageViews.add(imageView);
+        }
+
+    }
+
+    private class MyAdapter extends PagerAdapter {
+
+        @Override
+        public int getCount() {
+            return imageViews.size();
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = imageViews.get(position);
+            container.addView(imageView);
+            return imageView;
+        }
+
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+
 
     @OnClick(R.id.btn_start_main)
     public void onViewClicked() {
     }
+
 }
