039412efe1bf130d9aa819ae053434228727eeeb
JCrest <1554687045@qq.com>
2017-06-06 00:17:23 +0800

    将请求到并解析好的数据传递给左侧菜单

--- 北京新闻/src/main/java/com/example/jiangchuanfa/beijingnews2rd/Fragment/LeftMenuFragment.java
+++ 北京新闻/src/main/java/com/example/jiangchuanfa/beijingnews2rd/Fragment/LeftMenuFragment.java
@@ -1,19 +1,23 @@
 package com.example.jiangchuanfa.beijingnews2rd.Fragment;
 
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.jiangchuanfa.beijingnews2rd.Base.BaseFragment;
+import com.example.jiangchuanfa.beijingnews2rd.DoMain.NewsCenterBean;
+
+import java.util.List;
 
 /**
  * Created by crest on 2017/6/3.
- *
  */
 
 public class LeftMenuFragment extends BaseFragment {
     TextView textView;
+    private List<NewsCenterBean.DataBean> datas;
 
     @Override
     public View initView() {
@@ -29,5 +33,13 @@ public class LeftMenuFragment extends BaseFragment {
     public void initData() {
         super.initData();
         textView.setText("左侧菜单");
+    }
+
+    //为左侧菜单设置数据的方法
+    public void setData(List<NewsCenterBean.DataBean> datas) {
+        this.datas = datas;
+        for (int i = 0; i < datas.size(); i++) {
+            Log.e("TAG", "" + datas.get(i).getTitle());
+        }
     }
 }
--- 北京新闻/src/main/java/com/example/jiangchuanfa/beijingnews2rd/Pager/NewsPager.java
+++ 北京新闻/src/main/java/com/example/jiangchuanfa/beijingnews2rd/Pager/NewsPager.java
@@ -6,12 +6,16 @@ import android.util.Log;
 import android.view.Gravity;
 import android.widget.TextView;
 
+import com.example.jiangchuanfa.beijingnews2rd.Activity.MainActivity;
 import com.example.jiangchuanfa.beijingnews2rd.Base.BasePager;
 import com.example.jiangchuanfa.beijingnews2rd.DoMain.NewsCenterBean;
+import com.example.jiangchuanfa.beijingnews2rd.Fragment.LeftMenuFragment;
 import com.example.jiangchuanfa.beijingnews2rd.Utils.ConstantUtils;
 import com.google.gson.Gson;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
 
 import okhttp3.Call;
 
@@ -21,6 +25,8 @@ import okhttp3.Call;
 
 public class NewsPager extends BasePager {
     TextView textView;
+
+    public List<NewsCenterBean.DataBean> datas;
 
 
     public NewsPager(Context context) {
@@ -54,12 +60,12 @@ public class NewsPager extends BasePager {
                 .execute(new StringCallback() {
                     @Override
                     public void onError(Call call, Exception e, int id) {
-                        Log.e("TAG","请求失败=="+e.getMessage());
+                        Log.e("TAG", "请求失败==" + e.getMessage());
                     }
 
                     @Override
                     public void onResponse(String response, int id) {
-                        Log.e("TAG","请求成功=="+response);
+                        Log.e("TAG", "请求成功==" + response);
                         processData(response);
 
                     }
@@ -70,7 +76,19 @@ public class NewsPager extends BasePager {
 
     private void processData(String json) {
         NewsCenterBean newsCenterBean = new Gson().fromJson(json, NewsCenterBean.class);
-        Log.e("TAG","解析成功了哦=="+ newsCenterBean.getData().get(0).getChildren().get(0).getTitle());
+        Log.e("TAG", "解析成功了哦==" + newsCenterBean.getData().get(0).getChildren().get(0).getTitle());
+        datas = newsCenterBean.getData();
+
+        /**
+         * 将解析出来的数据传到左侧菜单
+         *
+         */
+        MainActivity mainActivity = (MainActivity) context;
+        //获取左侧菜单（mainActivity 继承了SlidingFragmentActivity 可通过字段LEFT_TAG在mainActivity获取左侧菜单）
+        LeftMenuFragment leftMenuFragment = mainActivity.getLeftMenuFragment();
+        //为左侧菜单设置数据
+        leftMenuFragment.setData(datas);
+
 
     }
 }
